cf1c45e574d7364d0c6e86807daa1994cced1039
Michelle Berry <mberry05@uoguelph.ca>
2024-03-06 11:01:50 -0500

    very basic drawing screen

--- lib/draw.dart
+++ lib/draw.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signature_pad/flutter_signature_pad.dart';
 
 class Draw extends StatefulWidget {
   const Draw({Key? key}) : super(key: key);
@@ -8,11 +9,75 @@ class Draw extends StatefulWidget {
 }
 
 class _DrawState extends State<Draw> {
+  final GlobalKey<SignatureState> _signatureKey = GlobalKey<SignatureState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("DRAW PAGE")
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Today's Prompt: Cats"),
+      ),
+      body: Column(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20.0),
+            child: Text(
+              'Draw Something!',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          const SizedBox(height: 50,),
+          Expanded(
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center, // Align children closer together
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Row(
+                        //mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              setState(() {
+                                _signatureKey.currentState?.clear();
+                              });
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              // Implement eraser tool if needed
+                            },
+                          ),
+                        ],
+                      ),
+                ),
+                Center(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: MediaQuery.of(context).size.width * 0.8,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: Signature(
+                      color: Colors.black,
+                      strokeWidth: 5.0,
+                      key: _signatureKey,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Implement finish action
+        },
+        child: Text('Done!'),
       ),
     );
   }
