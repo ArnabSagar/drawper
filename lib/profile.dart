80f61366bfb070fe08351848fe943c9ffa85f8e2
Austin Van Braeckel <avanbrae@uoguelph.ca>
2024-03-08 14:19:36 -0500

    Finished profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -2,6 +2,7 @@ import 'package:drawper/menudrawer.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 import 'feed.dart';
 import 'draw.dart';
@@ -17,6 +18,7 @@ class Profile extends StatefulWidget {
   _ProfileState createState() => _ProfileState();
 }
 
+// Generates the desired display of the given integer - eg. 4200 -> "4.2K"
 String getNumberDisplay(int number) {
   double numAsDouble = number.toDouble();
   String identifier = "";
@@ -35,6 +37,13 @@ String getNumberDisplay(int number) {
     return numAsDouble.toInt().toString() + identifier;
   } // otherwise show one decimal
   return numAsDouble.toStringAsFixed(1) + identifier;
+}
+
+// Generates the desired display of the given date, which must be in YYYY-MM-DD format
+String getDateDisplay(String date) {
+  DateTime dateTime = DateTime.parse(date);
+  String formattedDate = DateFormat('MMMM d, yyyy').format(dateTime);
+  return formattedDate;
 }
 
 class _ProfileState extends State<Profile> {
@@ -85,26 +94,29 @@ class _ProfileState extends State<Profile> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const SizedBox(height: 10), // spacer for aesthetics
+          const SizedBox(height: 5), // spacer for aesthetics
           Row( // username display
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text("@${_profileData['username']}", style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
             ]
           ),
-          const SizedBox(height: 10), // spacer for aesthetics
+          const SizedBox(height: 5), // spacer for aesthetics
           Row( // Profile picture plus user information
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const SizedBox(width: 5), // spacer for aesthetics
-              const SizedBox( // profile picture section
+              SizedBox( // profile picture section
                 height: 100,
                 width: 100,
                 child: DecoratedBox(
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    color: Colors.black,
+                    border: Border.all(color: Colors.purple.shade900, width: 3, strokeAlign: BorderSide.strokeAlignOutside),
+                    image: DecorationImage(
+                      image: NetworkImage(_profileData['profilePicUrl'])
+                    )
                   )
                 )
               ),
@@ -205,22 +217,74 @@ class _ProfileState extends State<Profile> {
                       itemCount: _profileData['posts'].length,
                       itemBuilder: (context, index) {
                         var post = _profileData['posts'][index];
-                        return ListTile( // an individual previous post
-                          title: Text(post['date'], style: const TextStyle(fontWeight: FontWeight.bold)),
-                          subtitle: Text(post['prompt'], style: const TextStyle(color: Colors.black)),
-                          trailing: Container(
-                            width: 60,
-                            decoration: const BoxDecoration( // a small preview image of the post
-                              color: Colors.grey,
-                              image: DecorationImage(
-                                image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6EdI9EP2OioyOrJNAmtb0N3CDsP_jB6w6Gw&usqp=CAU"), //TODO: CHANGE TO "post['image']" when we have images loaded via json
-                                fit: BoxFit.fill,
-                              ) 
+                        var viewsDisplay = getNumberDisplay(post['views']);
+                        var likesDisplay = getNumberDisplay(post['likes']);
+                        var dislikesDisplay = getNumberDisplay(post['dislikes']);
+                        var dateDisplay = getDateDisplay(post['date']);
+                        return Stack(
+                          children: [
+                            ListTile( // an individual previous post
+                              title: Text(dateDisplay, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                              subtitle: Text(post['prompt'], style: const TextStyle(color: Colors.black54, fontSize: 13)),
+                              trailing: Container(
+                                height: 60,
+                                width: 60,
+                                decoration: const BoxDecoration( // a preview image of the post
+                                  color: Colors.grey,
+                                  image: DecorationImage(
+                                    image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6EdI9EP2OioyOrJNAmtb0N3CDsP_jB6w6Gw&usqp=CAU"), //TODO: CHANGE TO "post['image']" when we have images loaded via json
+                                    fit: BoxFit.fill,
+                                  ) 
+                                ),
+                              ),
+                              onTap: () {
+                                // TODO BRINGS THEM TO A DETAILED PAGE ABOUT THE POST
+                              },
                             ),
-                          ),
-                          onTap: () {
-                            // TODO BRINGS THEM TO A DETAILED PAGE ABOUT THE POST
-                          },
+                            Row( // Post stats display - views, likes, dislikes
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const SizedBox(width: 100), // spacer for aesthetics
+                                const SizedBox(width: 100), // spacer for aesthetics
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        const Icon(Icons.remove_red_eye, color: Colors.black54, size: 15),
+                                        const SizedBox(width: 5),
+                                        Text(viewsDisplay, style: const TextStyle(color: Colors.black54, fontSize: 12))
+                                      ]
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Icon(Icons.thumb_up, color: Colors.green.shade600, size: 15),
+                                        const SizedBox(width: 5),
+                                        Text(likesDisplay, style: TextStyle(color: Colors.green.shade600, fontSize: 12))
+                                      ]
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Icon(Icons.thumb_down, color: Colors.red.shade700, size: 15),
+                                        const SizedBox(width: 5),
+                                        Text(dislikesDisplay, style: TextStyle(color: Colors.red.shade700, fontSize: 12))
+                                      ]
+                                    ),
+                                  ]
+                                ),
+                                const SizedBox(width: 100), // spacer for aesthetics
+                              ]
+                            )
+                          ]
                         );
                       }
                     )
