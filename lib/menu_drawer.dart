b2f3c98aaf32f77a74579557539e4f6681da6892
Austin Van Braeckel <avanbrae@uoguelph.ca>
2024-04-04 22:22:55 -0400

    Updated drawer menu

--- lib/menu_drawer.dart
+++ lib/menu_drawer.dart
@@ -2,11 +2,8 @@ import 'package:drawper/pages/draw_first.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import 'feed.dart';
-import 'profile.dart';
-import 'settings.dart';
 import 'main.dart';
-import 'pages/home_page.dart';
+import 'help.dart';
 
 class MenuDrawer extends StatelessWidget {
   const MenuDrawer({Key? key}) : super(key: key);
@@ -24,7 +21,7 @@ class MenuDrawer extends StatelessWidget {
                 color: Colors.purple.shade900,
               ),
               child: const Text(
-                'Drawper Menu',
+                'More Actions',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
@@ -35,68 +32,15 @@ class MenuDrawer extends StatelessWidget {
           ),
           ListTile(
             leading: const Icon(
-              Icons.list_alt_outlined,
+              Icons.help_center_outlined,
             ),
-            title: const Text('Feed'),
+            title: const Text('Help'),
             onTap: () {
-              Navigator.pop(context);
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Feed()));
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.person,
-            ),
-            title: const Text('Your @Profile'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Profile()));
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.person,
-            ),
-            title: const Text('Old Home Page/Menu (by austin)'),
-            onTap: () {
-              Navigator.pop(context);
               Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const HomePage(),
-                  )
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.settings,
-            ),
-            title: const Text('Settings'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const Settings(),
-                  )
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.settings,
-            ),
-            title: const Text('Draw First Info Page'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => DrawFirst()
-                  )
+                      builder: (context) =>
+                          const Help())
               );
             },
           ),
@@ -113,13 +57,6 @@ class MenuDrawer extends StatelessWidget {
                           const MyHomePage(title: "Drawper Login Page")),
                   (route) => false);
             },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.help_center_outlined,
-            ),
-            title: const Text('Help'),
-            onTap: () {},
           ),
         ],
       ),
